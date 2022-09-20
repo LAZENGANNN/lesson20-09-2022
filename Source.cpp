2be465f568b26ab387f0de871f2928fd9be2e36c
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-09-20 18:21:18 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,184 @@
+#include<iostream>
+using namespace std;
+int main() {
+	////integer1
+	//int L;
+	//cin >> L;
+	//int M = L / 100;
+	//cout << M;
+
+	//integer2
+	/*int M;
+	cin >> M;
+	int T = M / 1000;
+	cout << T;*/
+
+	//integer3
+	/*int B;
+	cin >> B;
+	int kB = B / 1024;
+	cout << kB;*/
+
+	//integer4
+	/*int A;
+	int B;
+	cin >> A >> B;
+	int K = A / B;
+	cout << K;*/
+
+	//integer5
+	/*int A;
+	int B;
+	cin >> A >> B;
+	int K = A % B;
+	cout << K;*/
+
+	//integer6
+	/*int N;
+	cin >> N;
+	int D = N / 10;
+	int E = N % 10;
+	cout << "desatki = " << D << " edenitsi = " << E;*/
+
+	//integer7
+	/*int N;
+	cin >> N;
+	int D = N / 10;
+	int E = N % 10;
+	int P = D * E;
+	cout << P;*/
+
+	//integer8
+	/*int N;
+	cin >> N;
+	int D = N / 10;
+	int E = N % 10;
+	cout << E << D;*/
+
+	//integer9
+	/*int N;
+	cin >> N;
+	int T = N / 100;
+	cout << T;*/
+
+	//integer10
+	/*int N;
+	cin >> N;
+	int E = N % 10;
+	int O = N % 100;
+	int D = O / 10;
+	cout << E << " " << D;*/
+
+	//integer11
+	/*int N;
+	cin >> N;
+	int E = N % 10;
+	int O = N % 100;
+	int D = O / 10;
+	int S = N / 100;
+	int P = E * D * S;
+	cout << P;*/
+
+	//integer12
+	/*int N;
+	cin >> N;
+	int E = N % 10;
+	int O = N % 100;
+	int D = O / 10;
+	int S = N / 100;
+	cout << E << D << S;*/
+
+	//integer13
+	/*int N;
+	cin >> N;
+	int E = N % 10;
+	int O = N % 100;
+	int D = O / 10;
+	int S = N / 100;
+	cout << D << E << S;*/
+
+	//integer14
+	/*int N;
+	cin >> N;
+	int E = N % 10;
+	int O = N % 100;
+	int D = O / 10;
+	int S = N / 100;
+	cout << E << S << D;*/
+
+    //integer15
+//int N;
+//cin >> N;
+//int E = N % 10;
+//int O = N % 100;
+//int D = O / 10;
+//int S = N / 100;
+//cout << D << S << E ;
+
+    //integer16
+//int N;
+//cin >> N;
+//int E = N % 10;
+//int O = N % 100;
+//int D = O / 10;
+//int S = N / 100;
+//cout << S << E << D;
+
+//integer17
+//int N;
+//cin >> N;
+//int S = N % 1000;
+//int D = S / 100;
+//cout << D;
+
+//integer18
+//int N;
+//cin >> N;
+//int T = N / 1000;
+//int T2 = T % 1;
+//cout << T;
+
+//integer19
+//int S;
+//cin >> S;
+//int M = S / 60;
+//cout << M;
+
+//integer20
+//int S;
+//cin >> S;
+//int h = S / 3600;
+//cout << h;
+
+//integer21
+//int S;
+//cin >> S;
+//int M = S / 60;
+//int L = M % 100;
+//cout << L;
+
+//integer22
+//int S;
+//cin >> S;
+//int h = S / 3600;
+//int L = h % 100;
+//cout << L;
+
+//integer23
+int S;
+cin >> S;
+int M = S / 60;
+int h = S / 3600;
+int L = h / 100;
+cout << L;
+
+
+
+
+
+
+
+
+
+	
+}
